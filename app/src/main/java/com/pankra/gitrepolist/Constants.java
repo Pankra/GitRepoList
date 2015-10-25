b640191153eb4e38c3d86b28efc3c594abc24469
Pankra <sergbk17@gmail.com>
2015-10-25 14:27:43 +0500

    Initial commit

---
+++ app/src/main/java/com/pankra/gitrepolist/Constants.java
@@ -0,0 +1,9 @@
+package com.pankra.gitrepolist;
+
+/**
+ * Created by Serge on 24.10.2015.
+ */
+public class Constants {
+    public static final String URL_USER = "https://api.github.com/users";
+    public static final String URL_USER_REPO = "https://api.github.com/users/{login}/repos";
+}
